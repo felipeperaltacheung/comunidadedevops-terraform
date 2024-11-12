d670ada8db5fa153a39705f29538bf062b0fcfa8
Felipe Cheung <f33___@hotmail.com>
2024-11-12 17:13:22 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,10 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.75.1"
     }
   }
+
+  backend "s3" {
+    bucket = "comunidadedevops2024"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
